ce51a3ebcb1ebdb850c82c724a28613f5a9d7cfd
ElhamHamid <69770702+ElhamHamid@users.noreply.github.com>
2020-09-09 01:09:17 -0500

    updated
    done

---
+++ Assignment2/filterLongWords/test.js
@@ -0,0 +1,15 @@
+"use strict";
+/* global filterLongWords assert */
+
+
+describe("filterLongWords",function() {
+    it("it returns words that have longest lengeth compered to the the given number",function() {
+        assert.equal(filterLongWords (["mark","jemal","yasmin"],4),"jemal","yasmin");
+    });   
+    it("it returns words that have longest lengeth compered to the the given number",function() {
+        assert.equal(filterLongWords (["hamid","said","mohammed"],5),"mohammed");
+    });   
+    it("it returns words that have longest lengeth compered to the the given number",function() {
+        assert.equal(filterLongWords (["aman","sol","mia"],3),"aman");
+    });   
+});
